53a45428e328fc0433d13ea6a8e2e47d449d8d84
prusama <marek.prusa.17@gmail.com>
2024-12-18 21:09:03 +0100

    Add http boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+const CURRENT_USER_URL = 'https://67631c3b17ec5852cae82142.mockapi.io/api/currentUser';
+const USER_FAVORITE_BOOKS_URL = 'https://67631c3b17ec5852cae82142.mockapi.io/api/usersFavoriteBooks';
+const PUBLISHING_COMPANIES_URL = 'https://67631c3b17ec5852cae82142.mockapi.io/api/publishingCompanies';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
 }
