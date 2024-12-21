7fac99de869a47d2ad63bc870337b8ca1a53c79c
prusama <marek.prusa.17@gmail.com>
2024-12-21 19:42:30 +0100

    Add urls to dummy endpoints

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
 
 const CURRENT_USER_URL = 'https://67631c3b17ec5852cae82142.mockapi.io/api/currentUser';
-const USER_FAVORITE_BOOKS_URL = 'https://67631c3b17ec5852cae82142.mockapi.io/api/usersFavoriteBooks';
+const USER_FAVORITE_BOOKS_URL = 'https://67631c3b17ec5852cae82142.mockapi.io/api/usersFavoriteBooks/*userId*';
 const PUBLISHING_COMPANIES_URL = 'https://67631c3b17ec5852cae82142.mockapi.io/api/publishingCompanies';
+const AUTH_URL = 'https://67631c3b17ec5852cae82142.mockapi.io/api/auth';
 
 @Component({
   selector: 'app-root',
